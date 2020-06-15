3553ce8bf11337989b2eed2b5cda215eef6cc22c
Khodidas_with_love_KD <41850210+Khodidas-withLoveKD@users.noreply.github.com>
2020-06-15 19:30:05 +0530

    After Sound Effects

--- 3_Project_Boost_2/Assets/rocket_engineering.cs
+++ 3_Project_Boost_2/Assets/rocket_engineering.cs
@@ -1,15 +1,30 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
+/* Difficulty level
+ * Increase difficulty -> increase thrust decrease Rotation
+ * Low difficulty: Thrust = 700; Rotation = 450
+ * Medium: Thrust = ; Rotation = ; 
+ * High: Thrust = ; Rotation = ;
+ */
 public class rocket_engineering : MonoBehaviour
 {
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float upThrust = 10f;
+    [SerializeField] AudioClip engineSound;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] AudioClip levelCompleteSound;
     Rigidbody rigidbody;
     AudioSource audioSource;
+    int totalLevels;
+
+    enum State {Alive, Dying, Transcending};
+    State state = State.Alive;      //default state
+
     // Start is called before the first frame update
     void Start()
     {
+        totalLevels = SceneManager.sceneCountInBuildSettings;
         rigidbody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
     }
@@ -17,23 +32,85 @@ public class rocket_engineering : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        if (state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
     }
 
-    private void ProcessInput()
+    private void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive)    //To stop the update method from being called multiple times
+            return;
+
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("Let's Begin");
+                break;
+
+            case "Finish":
+                state = State.Transcending;
+                audioSource.Stop();
+                audioSource.PlayOneShot(levelCompleteSound);
+                Invoke("LoadNextLevel", 1f);
+                break;
+
+            default:
+                // state = State.Dying;
+                audioSource.Stop();
+                audioSource.PlayOneShot(deathSound);
+                //Invoke("LoadSameLevel", 1f);
+                break;
+        }
+    }
+
+    private void LoadSameLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLevel()
+    {
+        int currentLevel = SceneManager.GetActiveScene().buildIndex;
+        print("Total Levels = "+totalLevels);
+        int nextLevel = (currentLevel + 1) % totalLevels;
+        SceneManager.LoadScene(nextLevel);
+    }
+
+    private void RespondToThrustInput()
+    {
+        float thrustInThisFrame = upThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidbody.AddRelativeForce(Vector3.up);
-            if(!audioSource.isPlaying)
-                audioSource.Play();
+            ApplyThrust(thrustInThisFrame);
         }
         else
-            audioSource.Pause();
+            audioSource.Stop();
+    }
+
+    private void ApplyThrust(float thrustInThisFrame)
+    {
+        rigidbody.AddRelativeForce(Vector3.up * thrustInThisFrame);
+        if (!audioSource.isPlaying)
+            audioSource.PlayOneShot(engineSound);
+    }
+
+    private void RespondToRotateInput()
+    {
+        float rotationInThisFrame = rcsThrust * Time.deltaTime;
+
+        rigidbody.freezeRotation = true;   //Rotate manually off
 
         if (Input.GetKey(KeyCode.A))
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationInThisFrame);
         else if (Input.GetKey(KeyCode.D))
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationInThisFrame);
+
+        rigidbody.freezeRotation = false;  //Set manual rotation on
     }
+
+    
 }
